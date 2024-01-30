9d1fc19b2a02b336c712fa78f979b6d7d379c234
Bduidan <dukovaleks2@gmail.com>
2024-01-30 22:39:10 +0300

    hihihaha

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
-console.log("Hello, мир!")
+let i = 0;
+const arr = [];
+
+setInterval(() => {
+    i++;
+    arr.push(i)
+
+    terminal.innerHTML = `
+${arr[arr.length - 1]} <br/>
+${arr[arr.length - 1]}\n
+`
+}, 1)
